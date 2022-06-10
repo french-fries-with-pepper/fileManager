30ff33d4a9aefc4d2dface6feef482937c1b9076
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-06-10 23:56:50 +0300

    feat: add mv and rm iplementation

--- src/builtinsApi.js
+++ src/builtinsApi.js
@@ -1,7 +1,7 @@
 import { printPrompt } from "./components/printPrompt.js";
 import { homedir } from "os";
 import * as path from "path";
-import { rename, writeFile, cp as copy } from "fs";
+import { rename, writeFile, cp as copy, rm as remove } from "fs";
 
 import { list } from "./builtins/ls.js";
 import { changeDir } from "./builtins/cd.js";
@@ -13,7 +13,18 @@ export class BuiltinsApi {
 
   constructor() {
     this.currentPath = homedir();
-    this.builtinsArr = ["up", "ls", "cd", "cat", "add", "hash", "rn", "cp"];
+    this.builtinsArr = [
+      "up",
+      "ls",
+      "cd",
+      "cat",
+      "add",
+      "hash",
+      "rn",
+      "cp",
+      "mv",
+      "rm",
+    ];
   }
 
   getPath() {
@@ -91,6 +102,30 @@ export class BuiltinsApi {
       path.join(args[1], path.basename(args[0]))
     );
     copy(oldPath, newPath, { recursive: true }, (err) => {
+      if (err) {
+        console.log("Operation failed");
+      }
+      printPrompt(this.currentPath);
+    });
+  }
+
+  async mv(args) {
+    const oldPath = this.parseFileName(this.currentPath, args[0]);
+    const newPath = this.parseFileName(
+      this.currentPath,
+      path.join(args[1], path.basename(args[0]))
+    );
+    rename(oldPath, newPath, (err) => {
+      if (err) {
+        console.log("Operation failed");
+      }
+      printPrompt(this.currentPath);
+    });
+  }
+
+  async rm(args) {
+    const pathToFile = this.parseFileName(this.currentPath, args[0]);
+    remove(pathToFile, {}, (err) => {
       if (err) {
         console.log("Operation failed");
       }
